7ad73e459d02fdae32208bf5c93a0a19b9d95685
Betsy Skinner <elizabeth.skinner@namely.com>
2018-09-26 09:49:03 -0400

    gradebook exercise

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
