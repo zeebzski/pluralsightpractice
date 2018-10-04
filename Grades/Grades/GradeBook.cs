9d191fad328ac38197f6ebd50b184850c9e7f42d
Betsy Skinner <elizabeth.skinner@namely.com>
2018-10-03 22:16:32 -0400

    class and object module, assembly module

--- Grades/Grades/GradeBook.cs
+++ Grades/Grades/GradeBook.cs
@@ -6,17 +6,38 @@ using System.Threading.Tasks;
 
 namespace Grades
 {
-    class GradeBook
+    public class GradeBook
     {
         public GradeBook()
         {
             grades = new List<float>();
         }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades;
+        public string Name;
+
+        private List<float> grades;
     }
 }
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,20 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello! This is the grade book program");
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            
+
         }
     }
 }
